18f5f7cd3fc2f13b4cc94da70a850c28bfb1a04f
Jmint <jeffreymintah737@gmail.com>
2023-12-18 00:08:10 +0000

    hope

--- DoIt/urls.py
+++ DoIt/urls.py
@@ -3,12 +3,10 @@ from .views import *
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('',StuffList.as_view(), name='stuffs'),
     path('login/',CustomLoginView.as_view(), name='login'),
     path('logout/',LogoutView.as_view(next_page = 'login'), name='logout'),
     path('register/',RegisterSection.as_view(), name='register'),
-
-
-    path('',StuffList.as_view(), name='stuffs'),
     path('task/<int:pk>', StuffDetail.as_view(), name='stuff'),
     path('create/', StuffCreate.as_view(), name='stuff-create'),
     path('update/<int:pk>', StuffUpdate.as_view(), name='stuff-update'),
